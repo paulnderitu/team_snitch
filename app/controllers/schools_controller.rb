bf78b979f4444b042e9f15c40c612ba3e70373bf
Your Name <youremail@domain.com>
2017-09-05 13:07:17 +0300

    defined a route for displaying schools. Also added controller actions for index, show and for adding a new school with all their views

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,2 +1,14 @@
 class SchoolsController < ApplicationController
+  def index
+    @schools = School.all
+  end
+
+  def show
+    @school = School.find(params[:id])
+  end
+
+  def new
+    @school = School.new
+    render :new
+  end
 end
